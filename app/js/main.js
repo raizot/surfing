14a01e05766cab75cb1498dfb63ef996bb25b7b6
George Izot <urka94@bk.ru>
2019-11-21 13:38:31 +0300

    изменения в разделе Surf

--- app/js/main.js
+++ app/js/main.js
@@ -5,10 +5,10 @@ $('.header__slider').slick({
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dotshead',
 });
 
-$('.slider-dots').slick({
+$('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
@@ -18,5 +18,13 @@ $('.surf-slider').slick({
     slidesToScroll: 1,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
-})
+    asNavFor: '.slider-map',
+});
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows:false,
+    asNavFor: '.surf-slider',
+    focusOnSelect:true
+});
 });
